e7f83b632e313e437d7fd92cfecfb094d90552d6
Bala <sbkrishnan2506@gmail.com>
2024-04-16 23:16:45 -0500

    Initial Commit

---
+++ Sample App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample App
+//
+//  Created by Balakrishnan Srinivasan on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to xcode cloud!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample App/Sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_AppApp.swift
+//  Sample App
+//
+//  Created by Balakrishnan Srinivasan on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
