d380f1183f967f376440dc4eb1229fa5b4d9169d
Bala <sbkrishnan2506@gmail.com>
2024-04-17 00:04:46 -0500

    test

--- Sample App/ContentView.swift
+++ Sample App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Welcome to xcode cloud!")
+            Text("Welcome to xcode cloud! ")
         }
         .padding()
     }
